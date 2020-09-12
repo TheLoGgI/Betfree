8b71ed1d6286ae78538d0d1258a5df623445d0d1
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2020-09-12 17:24:43 +0200

    Aside menu
    Aside menu bar navigation is active and functional

--- js/menuicon.js
+++ js/menuicon.js
@@ -1,13 +1,15 @@
     // "classToggle('menuIcon','icon-active') 
-    // classToggle('aside','open')
-window.addEventListener('DOMContentLoaded', () => {
-
-    clickHandler('menuIcon', 'icon-active')
+    // classToggle('asideNav','open')
+    window.addEventListener('DOMContentLoaded', () => {
+        clickHandler('menuIcon')
 })
 
 function clickHandler(elementID, className = 'active') {
     document.getElementById(elementID).addEventListener('click', _ => {
         classToggle(elementID, className)
+        // const hasClass = checkClass(document.getElementById(elementID), className)
+        classToggle('asideNav','open')
+
     }, false)
 }
 
@@ -15,3 +17,11 @@ function classToggle(elementID, className) {
     document.getElementById(elementID)
         .classList.toggle(className)
 }
+
+
+function checkClass(element, className) {
+    return [...element.classList].includes(className)
+
+}
+
+
