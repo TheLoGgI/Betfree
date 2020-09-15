ec655bed29cf85511d140b227d5d4b80682b6c49
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2020-09-15 10:40:08 +0200

    RandomDate

--- js/glider.js
+++ js/glider.js
@@ -9,3 +9,18 @@ const glide = new Glide('.glide', {
   })
 
   glide.mount()
+
+
+  function getRandomDate() {
+    const newDate = new Date()
+    newDate.setDate(randomInt(32))
+    newDate.setMonth(randomInt(12))
+    const date = newDate.toDateString().split(' ')
+    return `${date[0]}, ${date[2]}th of ${date[1]}`
+  }
+
+//   mon, 6 juli
+
+  function randomInt(max, min = 1) {
+      return Math.floor(Math.random() * max + min)
+  }
