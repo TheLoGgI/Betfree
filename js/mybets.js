1cfc10db4fa1c34928e6ff807f28d8bd3b24fb62
WiktorrWie <wiktor.wieczorek@interia.pl>
2020-09-17 09:23:16 +0200

    .

---
+++ js/mybets.js
@@ -0,0 +1,27 @@
+let bet1 = document.getElementById(coupon1);
+
+function cancel1() {
+    document.getElementById('coupon1').style.cssText = "display: none";
+    alert("coupon deleted!")
+}
+
+let bet2 = document.getElementById(coupon2);
+
+function cancel2() {
+    document.getElementById('coupon2').style.cssText = "display: none";
+    alert("coupon deleted!")
+}
+
+let bet3 = document.getElementById(coupon3);
+
+function cancel3() {
+    document.getElementById('coupon3').style.cssText = "display: none";
+    alert("coupon deleted!")
+}
+
+let bet4 = document.getElementById(coupon4);
+
+function cancel4() {
+    document.getElementById('coupon4').style.cssText = "display: none";
+    alert("coupon deleted!")
+}
--- js/placebet.js
+++ js/placebet.js
@@ -1,7 +1,13 @@
 
-let inputCoins = document.getElementById("input").value;
-document.getElementById("input").addEventListener("keyup", calculate);
 
-function calculate(e) {
-    console.log(e);
+document.getElementById("input").addEventListener("keyup", inputhandler);
+
+
+function inputhandler(e) {
+    const result = calculate()
+    document.getElementById("reward").innerHTML = result;
+}
+
+function calculate(inputCoins) {
+    return "Reward" + document.getElementById("input").value * 1.4 + "coins";
 }
