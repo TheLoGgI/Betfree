4e22b0a42ce0f27ea189baf6e937744497f578e2
WiktorrWie <wiktor.wieczorek@interia.pl>
2020-09-18 12:33:07 +0200

    .

--- js/matchpage.js
+++ js/matchpage.js
@@ -13,6 +13,82 @@ function removeAllClasses() {
     }
 }
 
-if (buttonsArray[0].classList.contains("chosenMatchOn")) {
-    document.getElementsByClassName("match-winner").style.cssText = "display: none;"
+let couponsArray = document.getElementsByClassName("match-winner");
+console.log(couponsArray);
+function popular() {
+    couponsArray[0].style.cssText = "display: block;"
+    couponsArray[1].style.cssText = "display: block;"
+    couponsArray[2].style.cssText = "display: block;"
+    couponsArray[3].style.cssText = "display: block;"
+    couponsArray[4].style.cssText = "display: none;"
+    couponsArray[5].style.cssText = "display: none;"
+    couponsArray[6].style.cssText = "display: none;"
+    couponsArray[7].style.cssText = "display: none;"
+    couponsArray[8].style.cssText = "display: none;"
+    couponsArray[9].style.cssText = "display: none;"
+    couponsArray[10].style.cssText = "display: none;"
+    couponsArray[11].style.cssText = "display: none;"
+    couponsArray[12].style.cssText = "display: none;"
+}
+
+function goals() {
+    couponsArray[0].style.cssText = "display: none;"
+    couponsArray[1].style.cssText = "display: block;"
+    couponsArray[2].style.cssText = "display: none;"
+    couponsArray[3].style.cssText = "display: none;"
+    couponsArray[4].style.cssText = "display: block;"
+    couponsArray[5].style.cssText = "display: block;"
+    couponsArray[6].style.cssText = "display: none;"
+    couponsArray[7].style.cssText = "display: none;"
+    couponsArray[8].style.cssText = "display: none;"
+    couponsArray[9].style.cssText = "display: none;"
+    couponsArray[10].style.cssText = "display: none;"
+    couponsArray[11].style.cssText = "display: none;"
+    couponsArray[12].style.cssText = "display: none;"
+}
+
+function corner() {
+    couponsArray[0].style.cssText = "display: none;"
+    couponsArray[1].style.cssText = "display: none;"
+    couponsArray[2].style.cssText = "display: none;"
+    couponsArray[3].style.cssText = "display: none;"
+    couponsArray[4].style.cssText = "display: none;"
+    couponsArray[5].style.cssText = "display: none;"
+    couponsArray[6].style.cssText = "display: block;"
+    couponsArray[7].style.cssText = "display: block;"
+    couponsArray[8].style.cssText = "display: none;"
+    couponsArray[9].style.cssText = "display: none;"
+    couponsArray[10].style.cssText = "display: none;"
+    couponsArray[11].style.cssText = "display: none;"
+    couponsArray[12].style.cssText = "display: none;"
+}
+function players() {
+    couponsArray[0].style.cssText = "display: none;"
+    couponsArray[1].style.cssText = "display: none;"
+    couponsArray[2].style.cssText = "display: none;"
+    couponsArray[3].style.cssText = "display: none;"
+    couponsArray[4].style.cssText = "display: none;"
+    couponsArray[5].style.cssText = "display: none;"
+    couponsArray[6].style.cssText = "display: none;"
+    couponsArray[7].style.cssText = "display: none;"
+    couponsArray[8].style.cssText = "display: block;"
+    couponsArray[9].style.cssText = "display: block;"
+    couponsArray[10].style.cssText = "display: none;"
+    couponsArray[11].style.cssText = "display: none;"
+    couponsArray[12].style.cssText = "display: none;"
+}
+function penalties() {
+    couponsArray[0].style.cssText = "display: none;"
+    couponsArray[1].style.cssText = "display: none;"
+    couponsArray[2].style.cssText = "display: none;"
+    couponsArray[3].style.cssText = "display: none;"
+    couponsArray[4].style.cssText = "display: none;"
+    couponsArray[5].style.cssText = "display: none;"
+    couponsArray[6].style.cssText = "display: none;"
+    couponsArray[7].style.cssText = "display: none;"
+    couponsArray[8].style.cssText = "display: none;"
+    couponsArray[9].style.cssText = "display: none;"
+    couponsArray[10].style.cssText = "display: block;"
+    couponsArray[11].style.cssText = "display: block;"
+    couponsArray[12].style.cssText = "display: block;"
 }
